7769c8d67884353441e80ad4eac0947ff895f32c
Bartlomiej Mika <bmika@icloud.com>
2015-02-15 17:51:29 -0500

    Upgraded agency css theme.

--- academicstoday_project/webapp/views.py
+++ academicstoday_project/webapp/views.py
@@ -20,7 +20,7 @@ from django.contrib.auth.decorators import login_required
 
 
 agency_css_library_urls = ["lib/jquery/1.11.1/jquery-ui.css",
-                           "lib/bootstrap/3.2.0/css/bootstrap.min.css",
+                           "lib/bootstrap/3.3.2/css/bootstrap.min.css",
                            "lib/font-awesome/4.1.0/css/font-awesome.css",
                            "lib/font-awesome/4.1.0/css/font-awesome.min.css",
                            "css/landpage.css",
@@ -36,20 +36,13 @@ agency_js_library_urls = ["lib/jquery/1.11.1/jquery.min.js",
                           "lib/jquery/1.11.1/jquery.tablesorter.js",
                           "lib/jquery/1.11.1/jquery-ui.js",
                           "lib/jquery-easing/1.3/jquery.easing.min.js",
-                          "lib/bootstrap/3.2.0/js/bootstrap.min.js",
-                          "lib/bootstrap/3.2.0/js/bootstrap.js",
-                          "lib/bootstrap/3.2.0/js/tab.js",
-                          "lib/bootstrap/3.2.0/js/popover.js",
-                          "lib/bootstrap/3.2.0/js/tooltip.js",
-                          "lib/bootstrap/3.2.0/js/button.js",
-                          "lib/bootstrap/3.2.0/js/modal.js",
-                          "lib/bootstrap/3.2.0/js/functions.js",
-                          "lib/bootstrap/3.2.0/js/collapse.js",
-                          "lib/bootstrap/3.2.0/js/transition.js",
+                          "lib/bootstrap/3.3.2/js/bootstrap.min.js",
+                          "lib/bootstrap/3.3.2/js/bootstrap.js",
                           "lib/classie/1.0.0/classie.js",
                           "lib/cbpanimatedheader/1.0.0/cbpAnimatedHeader.js",
                           "lib/cbpanimatedheader/1.0.0/cbpAnimatedHeader.min.js",
-                          "lib/jqbootstrapvalidation/1.3.6/jqBootstrapValidation.js"]
+                          "lib/jqbootstrapvalidation/1.3.6/jqBootstrapValidation.js",
+                          "lib/misc/agency.js"]
 
 sb_admin_js_library_urls = ["lib/jquery/1.11.1/jquery.min.js",
                             "lib/jquery/1.11.1/jquery.tablesorter.js",
