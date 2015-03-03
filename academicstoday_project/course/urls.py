726d1073d10d623003fa8479f53fef27d64b6897
Bartlomiej Mika <bmika@icloud.com>
2015-03-03 12:42:34 -0500

    #17 Latest progress with developing exams UI. Added minor fixes.

--- academicstoday_project/course/urls.py
+++ academicstoday_project/course/urls.py
@@ -6,15 +6,21 @@ from . import views
 from course.views import course
 
 urlpatterns = patterns('',
+    # Announcement
     url(r'^course/(\d)/$', course.course),
     url(r'^course/(\d)/home$', course.course_home),
+  
+    # Syllabus
     url(r'^course/(\d)/syllabus$', course.course_syllabus),
+   
+    # Grades & Policy
     url(r'^course/(\d)/policy$', course.course_policy),
+
+    # Lecture
     url(r'^course/(\d)/lectures$', course.course_lectures),
     url(r'^course/(\d)/lecture$', course.lecture),
-    url(r'^course/(\d)/quizzes$', course.course_quizzes),
-    url(r'^course/(\d)/exams$', course.course_exams),
-    url(r'^course/(\d)/discussion$', course.course_discussion),
+                       
+    # Assignment
     url(r'^course/(\d)/assignments$', course.assignments),
     url(r'^course/(\d)/assignment_delete$', course.assignment_delete),
     url(r'^course/(\d)/assignment_essay$', course.assignment_essay),
@@ -28,8 +34,21 @@ urlpatterns = patterns('',
     url(r'^course/(\d)/submit_mc_assignment_completion$', course.submit_mc_assignment_completion),
     url(r'^course/(\d)/submit_truefalse_assignment_completion$', course.submit_truefalse_assignment_completion),
     url(r'^course/(\d)/submit_response_assignment_completion$', course.submit_response_assignment_completion),
+                       
+    # Quiz
+    url(r'^course/(\d)/quizzes$', course.course_quizzes),
     url(r'^course/(\d)/quiz_truefalse$', course.quiz_truefalse),
     url(r'^course/(\d)/submit_truefalse_quiz_answer$', course.submit_truefalse_quiz_answer),
     url(r'^course/(\d)/submit_truefalse_quiz_completion$', course.submit_truefalse_quiz_completion),
     url(r'^course/(\d)/quiz_delete$', course.quiz_delete),
+    
+    # Exam
+    url(r'^course/(\d)/exams$', course.course_exams),
+    url(r'^course/(\d)/exam_multiplechoice$', course.exam_multiplechoice),
+    url(r'^course/(\d)/submit_mc_exam_answer$', course.submit_mc_exam_answer),
+    url(r'^course/(\d)/submit_mc_exam_completion$', course.submit_mc_exam_completion),
+    url(r'^course/(\d)/exam_delete$', course.exam_delete),
+                       
+    # Discussion
+    url(r'^course/(\d)/discussion$', course.course_discussion),
 )
--- academicstoday_project/course/views/course.py
+++ academicstoday_project/course/views/course.py
@@ -19,6 +19,8 @@ from course.models import TrueFalseQuestion
 from course.models import TrueFalseSubmission
 from course.models import Quiz
 from course.models import QuizSubmission
+from course.models import Exam
+from course.models import ExamSubmission
 import json
 import datetime
 from django.http import HttpResponse
@@ -271,7 +273,7 @@ def assignment_delete(request, course_id):
                         exam_id=0,
                     ).delete()
                     response_data = {'status' : 'success', 'message' : 'assignment was deleted'}
-                except EssaySubmission.DoesNotExist:
+                except MultipleChoiceSubmission.DoesNotExist:
                     response_data = {'status' : 'failed', 'message' : 'assignment not found'}
             elif assignment_type == settings.TRUEFALSE_ASSIGNMENT_TYPE:
                 try:
@@ -282,7 +284,7 @@ def assignment_delete(request, course_id):
                         quiz_id=0,
                     ).delete()
                     response_data = {'status' : 'success', 'message' : 'assignment was deleted'}
-                except EssaySubmission.DoesNotExist:
+                except TrueFalseSubmission.DoesNotExist:
                     response_data = {'status' : 'failed', 'message' : 'assignment not found'}
             elif assignment_type == settings.RESPONSE_ASSIGNMENT_TYPE:
                 try:
@@ -292,7 +294,7 @@ def assignment_delete(request, course_id):
                         course_id=course_id,
                     ).delete()
                     response_data = {'status' : 'success', 'message' : 'assignment was deleted'}
-                except EssaySubmission.DoesNotExist:
+                except ResponseSubmission.DoesNotExist:
                     response_data = {'status' : 'failed', 'message' : 'assignment not found'}
             else:
                 response_data = {'status' : 'success', 'message' : ''}
@@ -631,13 +633,13 @@ def submit_response_assignment_completion(request, course_id):
 def course_quizzes(request, course_id):
     course = Course.objects.get(id=course_id)
     
-    # Fetch all the assignments for this course.
+    # Fetch all the quizzes for this course.
     try:
         quizzes = Quiz.objects.filter(course_id=course_id).order_by('order_num')
-    except Assignment.DoesNotExist:
+    except Quiz.DoesNotExist:
         quizzes = None
 
-    # Fetch all submitted assignments
+    # Fetch all submitted quizzes
     try:
         submitted_quizzes = QuizSubmission.objects.filter(course_id=course_id,
                                                           student_id=request.user.id)
@@ -809,13 +811,188 @@ def quiz_delete(request, course_id):
 @login_required(login_url='/landpage')
 def course_exams(request, course_id):
     course = Course.objects.get(id=course_id)
-    return render(request, 'course/exam/exams.html',{
+    
+    # Fetch all the assignments for this course.
+    try:
+        exams = Exam.objects.filter(course_id=course_id).order_by('order_num')
+    except Exam.DoesNotExist:
+        exams = None
+    
+    # Fetch all submitted assignments
+    try:
+        submitted_exams = ExamSubmission.objects.filter(course_id=course_id,
+                                                        student_id=request.user.id)
+    except ExamSubmission.DoesNotExist:
+        submitted_exams = None
+    
+    # If the submissions & quizzes counts do not equal, then we have to
+    # iterate through all the quizzes and create the missing 'submission'
+    # entries for our system.
+    if len(exams) != len(submitted_exams):
+        for exam in exams:
+            found_exam = False
+            for submitted_exam in submitted_exams:
+                if exam.id == submitted_exam.exam_id:
+                    found_exam = True
+            if not found_exam:
+                submission = ExamSubmission.create(
+                    student_id=request.user.id,
+                    course_id=course_id,
+                    exam_id=exam.id,
+                    type=exam.type,
+                    order_num=exam.order_num
+                )
+                submission.save()
+
+    return render(request, 'course/exam/list.html',{
         'course' : course,
         'user' : request.user,
+        'exams' : exams,
+        'submitted_exams' : submitted_exams,
+        'ESSAY_ASSIGNMENT_TYPE' : settings.ESSAY_ASSIGNMENT_TYPE,
+        'MULTIPLECHOICE_ASSIGNMENT_TYPE' : settings.MULTIPLECHOICE_ASSIGNMENT_TYPE,
+        'TRUEFALSE_ASSIGNMENT_TYPE' : settings.TRUEFALSE_ASSIGNMENT_TYPE,
+        'RESPONSE_ASSIGNMENT_TYPE' : settings.RESPONSE_ASSIGNMENT_TYPE,
         'tab' : 'exams',
+        'subtab' : '',
         'local_css_urls' : css_library_urls,
-        'local_js_urls' : js_library_urls
+        'local_js_urls' : js_library_urls,
     })
+
+
+@login_required()
+def exam_multiplechoice(request, course_id):
+    if request.is_ajax():
+        if request.method == 'POST':
+            exam_id = int(request.POST['exam_id'])
+            exam = Exam.objects.get(id=exam_id)
+            try:
+                questions = MultipleChoiceQuestion.objects.filter(
+                    assignment_id=exam_id,
+                    course_id=course_id,
+                    exam_id=0,
+                )
+            except MultipleChoiceQuestion.DoesNotExist:
+                questions = None
+            
+            return render(request, 'course/exam/mc_modal.html',{
+                'exam' : exam,
+                'questions' : questions,
+            })
+
+
+@login_required()
+def submit_mc_exam_completion(request, course_id):
+    # Update the 'submission_date' of our entry to indicate we
+    # have finished the exam.
+    submission = ExamSubmission.objects.get(
+        exam_id=int(request.POST['exam_id']),
+        student_id=int(request.POST['student_id']),
+        course_id=int(request.POST['course_id']),
+    )
+    submission.submission_date = datetime.datetime.utcnow()
+    submission.save()
+                                                  
+    response_data = {'status' : 'success', 'message' : ''}
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+
+@login_required()
+def submit_mc_exam_answer(request, course_id):
+    if request.is_ajax():
+        if request.method == 'POST':
+            exam_id = int(request.POST['exam_id'])
+            student_id = int(request.POST['student_id'])
+            course_id = int(request.POST['course_id'])
+            question_num = int(request.POST['num'])
+            key = request.POST['key']
+            value = request.POST['value']
+            # Fetch question and error if not found.
+            try:
+                question = MultipleChoiceQuestion.objects.get(
+                    assignment_id=0,
+                    course_id=course_id,
+                    question_num=question_num,
+                    exam_id=exam_id,
+                )
+            except MultipleChoiceQuestion.DoesNotExist:
+                response_data = {'status' : 'failed', 'message' : 'cannot find question'}
+                return HttpResponse(json.dumps(response_data), content_type="application/json")
+        
+            # Fetch submission and create new submission if not found.
+            try:
+                submission = MultipleChoiceSubmission.objects.get(
+                    student_id=student_id,
+                    assignment_id=0,
+                    course_id=course_id,
+                    question_num=question_num,
+                    exam_id=exam_id,
+                )
+            except MultipleChoiceSubmission.DoesNotExist:
+                submission = MultipleChoiceSubmission.create(
+                    student_id=student_id,
+                    assignment_id=0,
+                    course_id=course_id,
+                    question_num=question_num,
+                    exam_id=exam_id,
+                )
+                submission.save()
+            
+            # Convert JSON string into Python array
+            answers = json.loads(submission.json_answers)
+            
+            # Append or remove the answers json entry from the submission object.
+            found_value = answers.get(key, None)
+            if found_value == value:
+                answers.pop(key, None)
+            else:
+                answers[key] = value
+            
+            # Convert back into JSON string and save
+            submission.json_answers = json.dumps(answers)
+            submission.save()
+            
+            response_data = {'status' : 'success', 'message' : ''}
+            return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+    response_data = {'status' : 'failed', 'message' : 'error submitting'}
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+
+@login_required()
+def exam_delete(request, course_id):
+    if request.is_ajax():
+        if request.method == 'POST':
+            student_id = int(request.POST['student_id'])
+            exam_id = int(request.POST['exam_id'])
+            exam_type = int(request.POST['exam_type'])
+            
+            # Update the 'submission_date' of our entry to indicate we
+            # have finished the exam.
+            submission = ExamSubmission.objects.get(
+                exam_id=int(request.POST['exam_id']),
+                student_id=int(request.POST['student_id']),
+                course_id=int(request.POST['course_id'])
+            )
+            submission.submission_date = None
+            submission.save()
+
+            if exam_type == settings.MULTIPLECHOICE_ASSIGNMENT_TYPE:
+                try:
+                    MultipleChoiceSubmission.objects.filter(
+                        assignment_id=0,
+                        exam_id=exam_id,
+                        student_id=student_id,
+                        course_id=course_id,
+                    ).delete()
+                    response_data = {'status' : 'success', 'message' : 'assignment was deleted'}
+                except MultipleChoiceSubmission.DoesNotExist:
+                    response_data = {'status' : 'failed', 'message' : 'assignment not found'}
+            else:
+                response_data = {'status' : 'success', 'message' : ''}
+            return HttpResponse(json.dumps(response_data), content_type="application/json")
+    response_data = {'status' : 'failed', 'message' : 'unknown error with deletion'}
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
 
 
 @login_required(login_url='/landpage')
