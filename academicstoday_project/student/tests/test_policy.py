aad6bee3267aac886b0148de6d88d7252db125f8
Bartlomiej Mika <bmika@icloud.com>
2015-03-31 11:58:22 -0400

    Policy and Grades now has unit tests. #65

--- academicstoday_project/student/tests/test_policy.py
+++ academicstoday_project/student/tests/test_policy.py
@@ -1,3 +1,70 @@
+# Django & Python
+from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+from django.http import QueryDict
 from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.conf.urls.static import static, settings
+import json
+
+# Modal
+from registrar.models import Course
+from registrar.models import Student
+from registrar.models import Policy
+
+
+# View
+from student.views import policy
+
+# Contants
+TEST_USER_EMAIL = "ledo@gah.com"
+TEST_USER_USERNAME = "Ledo"
+TEST_USER_PASSWORD = "password"
+
+# Notes:
+# https://docs.djangoproject.com/en/1.7/topics/testing/tools/#assertions
 
 # Create your tests here.
+class PolicyTestCase(TestCase):
+    def setUp(self):
+        # Create our Student.
+        User.objects.create_user(
+            email=TEST_USER_EMAIL,
+            username=TEST_USER_USERNAME,
+            password=TEST_USER_PASSWORD
+        ).save()
+        user = User.objects.get(email=TEST_USER_EMAIL)
+        Student.objects.create(user=user).save()
+                                 
+        # Create a test course.
+        Course.objects.create(
+            id=1,
+            title="Comics Book Course",
+            sub_title="The definitive course on comics!",
+            category="",
+        )
+
+    def get_logged_in_client(self):
+        client = Client()
+        client.login(
+            username=TEST_USER_USERNAME,
+            password=TEST_USER_PASSWORD
+        )
+        return client
+
+    def test_url_resolves_to_policy_page_view(self):
+        found = resolve('/course/1/policy')
+        self.assertEqual(found.func, policy.policy_page)
+    
+    def test_policy_page_with_correct_html(self):
+        client = self.get_logged_in_client()
+        response = client.post('/course/1/policy')
+        
+        # Verify we are in the correct course.
+        self.assertIn(b'Comics Book Course',response.content)
+        
+        # Verify
+        self.assertIn(b'<h1>Policy and Grades</h1>',response.content)
