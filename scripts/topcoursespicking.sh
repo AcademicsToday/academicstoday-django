35a6f4aa3164e419d70aad1cf3c3bdfcad472637
Bartlomiej Mika <bmika@icloud.com>
2015-04-09 13:24:59 -0400

    Wrote command for top source picking.

---
+++ scripts/topcoursespicking.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#------------------------------------------------------------------------------#
+# topcoursepicking.sh                                                          #
+#------------------------------------------------------------------------------#
+# By: Bartlomiej Mika
+# Date: April, 8th, 2015
+#
+# Description:
+# This script will load up the django virtual environment instance
+# and run the 'topcoursespicking' command.
+#
+# Prerequsites:
+# (1) Running on FreeBSD OS
+# (2) Running in user called 'freebsd'
+# (3) AcademicsToday dir: /usr/home/freebsd/py-academicstoday
+#
+#----------------#
+# HOWTO: Setup:  #
+#----------------#
+# (1) Give permission
+# $ chmod u+x /usr/home/freebsd/py-academicstoday/scripts/topcoursepicking.sh
+#
+# (1) Run command:
+# $ cron -e
+#
+# (2) This makes cron job run once a month:
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# 0 0 1 * * ./usr/home/freebsd/py-academicstoday/scripts/topcoursepicking.sh
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Clear all text on the screen
+clear;
+
+# Turn on virtual environment
+source /usr/home/freebsd/py-academicstoday/env/bin/activate.csh
+
+# Run command
+cd /usr/home/freebsd/py-academicstoday/
+python manage.py topcoursespicking
+
+# Turn off virtual environment
+deactivate
