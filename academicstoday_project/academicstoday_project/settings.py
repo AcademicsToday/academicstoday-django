a971f51cb19844af54da5597a8766e848322d12e
Bartlomiej Mika <bmika@icloud.com>
2015-04-15 13:29:19 -0400

    Unit tests pass with Captchas. #96

--- academicstoday_project/academicstoday_project/settings.py
+++ academicstoday_project/academicstoday_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import sys
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -76,6 +77,12 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'academicstoday_project.wsgi.application'
+
+
+# Captcha App
+
+if 'test' in sys.argv:
+    CAPTCHA_TEST_MODE = True
 
 
 # Database
--- academicstoday_project/account/tests/test_registration.py
+++ academicstoday_project/account/tests/test_registration.py
@@ -8,6 +8,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 import json
 from account.views import registration
+from captcha.models import CaptchaStore
 
 # Contants
 TEST_USER_EMAIL = "ledo@gah.com"
@@ -20,7 +21,8 @@ class RegistrationTestCase(TestCase):
         User.objects.all().delete()
     
     def setUp(self):
-        pass
+        captcha_count = CaptchaStore.objects.count()
+        self.failUnlessEqual(captcha_count, 0)
 
     def test_url_resolves_to_register(self):
         found = resolve('/register')
@@ -30,6 +32,10 @@ class RegistrationTestCase(TestCase):
         # Extra parameters to make this a Ajax style request.
         kwargs = {'HTTP_X_REQUESTED_WITH':'XMLHttpRequest'}
         
+        # Developer Notes:
+        # To get unit tests working with the django-simple-captcha then follow:
+        # http://stackoverflow.com/questions/3159284/how-to-unit-test-a-form-with-a-captcha-field-in-django
+        
         # Test
         client = Client()
         response = client.post('/register',{
@@ -38,8 +44,10 @@ class RegistrationTestCase(TestCase):
             'password_repeated': TEST_USER_PASSWORD,
             'first_name': 'Ledo',
             'last_name': 'Dunno',
-            'email': TEST_USER_EMAIL,
+            'electronic_mail': TEST_USER_EMAIL,
             'is_18_or_plus': True,
+            'captcha_0': 'dummy-value',
+            'captcha_1': 'PASSED',
         },**kwargs)
         
         # Verify: Check that the response is 200 OK.
@@ -48,8 +56,8 @@ class RegistrationTestCase(TestCase):
         # Verify: Successful response.
         json_string = response.content.decode(encoding='UTF-8')
         array = json.loads(json_string)
-        self.assertEqual(array['status'], 'success')
         self.assertEqual(array['message'], 'successfully registered')
+        self.assertEqual(array['status'], 'success')
 
         # Verify: Database updated
         try:
