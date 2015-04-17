9b393ecf4e5a32e1d4466ef1df1b31d6e664f551
Bartlomiej Mika <bmika@icloud.com>
2015-04-17 13:37:32 -0400

    BUGFIX in Account.profiles section. #105

--- academicstoday_project/account/forms.py
+++ academicstoday_project/account/forms.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django import forms
-
-from django.forms import ModelForm
+from django.conf import settings
+from django.forms import ModelForm, TextInput
+from django.contrib.auth.models import User
 from account.models import PrivateMessage
 
 
@@ -12,3 +13,48 @@ class PrivateMessageForm(forms.ModelForm):
         labels = {
             'from_address': 'From',
         }
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+
+        widgets = {
+            'first_name': TextInput(attrs={'class': u'form-control','placeholder': u'Enter First Name', 'required': True}),
+            'last_name': TextInput(attrs={'class': u'form-control','placeholder': u'Enter Last Name', 'required': True}),
+            'email': TextInput(attrs={'class': u'form-control','placeholder': u'Enter Email', 'required': True}),
+        }
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data['first_name']
+        if first_name is not None and first_name is not '':
+            return first_name
+        else:
+            raise forms.ValidationError("Cannot be blank")
+
+    def clean_last_name(self):
+        last_name = self.cleaned_data['last_name']
+        if last_name is not None and last_name is not '':
+            return last_name
+        else:
+            raise forms.ValidationError("Cannot be blank")
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if email is not None and email is not '':
+            return email
+        else:
+             raise forms.ValidationError("Cannot be blank")
+
+    def clean(self):
+        cleaned_data = super(UserForm, self).clean()
+        
+        # Verify no duplicate emails occur.
+        try:
+            email = cleaned_data.get("email")
+            user = User.objects.get(email=email)
+            if user != self.instance:
+                raise forms.ValidationError("Email already exists")
+        except User.DoesNotExist:
+            pass
+
--- academicstoday_project/account/views/profile.py
+++ academicstoday_project/account/views/profile.py
@@ -1,23 +1,23 @@
 from django.shortcuts import render
-from django.core import serializers
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 import json
-import datetime
 from registrar.models import Student
 from registrar.models import Course
 from registrar.models import Announcement
+from account.forms import UserForm
 
 
 @login_required(login_url='/landpage')
 def profile_page(request):
     return render(request, 'account/profile/view.html',{
-        'user' : request.user,
-        'tab' : 'profile',
-        'local_css_urls' : settings.SB_ADMIN_2_CSS_LIBRARY_URLS,
-        'local_js_urls' : settings.SB_ADMIN_2_JS_LIBRARY_URLS,
+        'user': request.user,
+        'form': UserForm(instance=request.user),
+        'tab': 'profile',
+        'local_css_urls': settings.SB_ADMIN_2_CSS_LIBRARY_URLS,
+        'local_js_urls': settings.SB_ADMIN_2_JS_LIBRARY_URLS,
     })
 
 
@@ -26,26 +26,11 @@ def update_user(request):
     response_data = {'status' : 'failed', 'message' : 'unknown deletion error'}
     if request.is_ajax():
         if request.method == 'POST':
-            first_name = request.POST['first_name']
-            last_name = request.POST['last_name']
-            email = request.POST['email']
-            
-            # Validate email.
-            # Check to see if we already have the username or email taken.
-            try:
-                user = User.objects.get(email__exact=request.POST['email'])
-                if user != request.user:
-                    response_data = {'status' : 'failure', 'message' : 'Email already exists, please choose another email' }
-                    return HttpResponse(json.dumps(response_data), content_type="application/json")
-            except User.DoesNotExist:
-                user = request.user
-
-            # Update model
-            user.first_name = first_name
-            user.last_name = last_name
-            user.email = email
-            user.username = email
-            user.save()
-
-            response_data = {'status' : 'success', 'message' : 'updated user'}
+            form = UserForm(instance=request.user, data=request.POST)
+            if form.is_valid():
+                form.instance.username = form.instance.email
+                form.save()
+                response_data = {'status' : 'success', 'message' : 'updated user'}
+            else:
+                response_data = {'status' : 'failed', 'message' : json.dumps(form.errors)}
     return HttpResponse(json.dumps(response_data), content_type="application/json")
