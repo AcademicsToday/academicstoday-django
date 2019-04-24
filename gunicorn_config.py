1f5048a27401ebc5cc255e2e7f6e703249c7d4c0
bartmika <bart@mikasoftware.com>
2019-04-24 15:31:33 -0400

    BUGFIX: Fixed bug with the gunicorn config.

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,4 @@
 command = '/opt/django/academicstoday-django/env/bin/gunicorn'
-pythonpath = '/opt/django/academicstoday-django/academicstoday'
+pythonpath = '/opt/django/academicstoday-django/academicstoday_project'
 bind = '127.0.0.1:8001'
 workers = 3
